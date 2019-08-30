6c5c360abe0b8e2bfa29c308942bcc67472f1cb3
Brock Cowen <brock12cowen@gmail.com>
2019-08-30 15:29:09 -0500

    hopefully fixed mobile

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var math = "";
 
 $(".grid")
-.mousedown(function(){
+.on('mousedown', function(){
     math = $(this).text();
     console.log(math);
     var ans = $(this).attr("value");
@@ -9,7 +9,7 @@ $(".grid")
     $(this).html(`<p style="color: white">${ans}</p>`);
     $(this).css('background-color', 'pink');
 })
-.mouseup(function() {
+.on('mouseup', function() {
     $(this).css('background-color', 'white');
     $(this).html(`<p style="color: black">${math}</p>`);
   })
