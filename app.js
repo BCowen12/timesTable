20734ee90d04e5c8182095c0685968c4af28c5e5
Brock Cowen <brock12cowen@gmail.com>
2019-08-30 15:41:31 -0500

    again hopefully fixed mobile

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
+$(document).ready(function () {
+    var isTouchDevice = 'ontouchstart' in document.documentElement;
+    
+    $(".grid")
+        .on('mousedown', function () {
+            if (isTouchDevice == false) {
+                math = $(this).text();
+                console.log(math);
+                var ans = $(this).attr("value");
+                console.log(ans);
+                $(this).html(`<p style="color: white">${ans}</p>`);
+                $(this).css('background-color', 'pink');
+            }
+        })
+        .on('mouseup', function () {
+            if (isTouchDevice == false) {
+                $(this).css('background-color', 'white');
+                $(this).html(`<p style="color: black">${math}</p>`);
+            }
+        })
+
+    $(".grid")
+        .on('touchstart', function () {
+            if (isTouchDevice) {
+                math = $(this).text();
+                console.log(math);
+                var ans = $(this).attr("value");
+                console.log(ans);
+                $(this).html(`<p style="color: white">${ans}</p>`);
+                $(this).css('background-color', 'pink');
+            }
+        })
+        .on('touchend', function () {
+            if (isTouchDevice) {
+                $(this).css('background-color', 'white');
+                $(this).html(`<p style="color: black">${math}</p>`);
+            }
+        });
+});
+
+
 var math = "";
 
-$(".grid")
-.on('mousedown', function(){
-    math = $(this).text();
-    console.log(math);
-    var ans = $(this).attr("value");
-    console.log(ans);
-    $(this).html(`<p style="color: white">${ans}</p>`);
-    $(this).css('background-color', 'pink');
-})
-.on('mouseup', function() {
-    $(this).css('background-color', 'white');
-    $(this).html(`<p style="color: black">${math}</p>`);
-  })
